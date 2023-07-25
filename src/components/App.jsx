74ee5b5bee583217cb37e1514fc8b1570748325f
irysik123 <vysotskaya.iryna@gmail.com>
2023-07-25 23:04:19 +0400

    deleting comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,22 +11,21 @@ export default function App() {
 
   const ADDED_CONTACTS = 'addedContacts';
 
-
   useEffect(() => {
     let savedContacts = localStorage.getItem(ADDED_CONTACTS);
     if (savedContacts) {
       savedContacts = JSON.parse(savedContacts);
       setContacts(savedContacts);
     } else {
-      setContacts(contactList)
+      setContacts(contactList);
     }
   }, []);
 
   useEffect(() => {
-    if(contacts) {
-    localStorage.setItem (ADDED_CONTACTS, JSON.stringify(contacts))
+    if (contacts) {
+      localStorage.setItem(ADDED_CONTACTS, JSON.stringify(contacts));
     }
-  }, [contacts])
+  }, [contacts]);
 
   const handleFilterChange = e => {
     setFilter(e.target.value);
@@ -50,8 +49,8 @@ export default function App() {
     let newContacts = contacts;
     newContacts.splice(indexToDelete, 1);
     // localStorage.setItem (ADDED_CONTACTS, JSON.stringify(newContacts))
-    setContacts([...newContacts]); 
-    // without ... React will not rerender component 
+    setContacts([...newContacts]);
+    // without ... React will not rerender component
   };
 
   return (
@@ -60,77 +59,15 @@ export default function App() {
       <ContactForm handleAdd={handleAdd} />
 
       <Title>Contacts</Title>
-      <Filter
-        handleFilterChange={handleFilterChange}
-        value={filter}
-      />
-      {contacts && <ContactList
-        list={contacts.filter(user =>
-          user.name.toLowerCase().includes(filter.toLowerCase())
-        )}
-        onDeleteContact={onDeleteContact}
-      />}
+      <Filter handleFilterChange={handleFilterChange} value={filter} />
+      {contacts && (
+        <ContactList
+          list={contacts.filter(user =>
+            user.name.toLowerCase().includes(filter.toLowerCase())
+          )}
+          onDeleteContact={onDeleteContact}
+        />
+      )}
     </div>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     contacts: contacts,
-//     filter: '',
-//   };
-
-//   handleFilterChange = e => {
-//     this.setState({ filter: e.target.value });
-//   };
-
-//   componentDidMount() {
-//     let savedContacts = localStorage.getItem(ADDED_CONTACTS)
-//     if (savedContacts) {
-//       savedContacts = JSON.parse(savedContacts);
-//       this.setState({ contacts: savedContacts})
-//     }
-//   }
-
-//   componentDidUpdate(_, prevState) {
-//     if(prevState.contacts !== this.state.contacts) {
-//       localStorage.setItem (ADDED_CONTACTS, JSON.stringify(this.state.contacts))
-//     }
-//   }
-
-//   handleAdd = newContact => {
-//     if(this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
-//       alert(`${newContact.name} is already in contacts`)
-//     } else {
-//       this.setState(({ contacts }) => ({
-//         contacts: [newContact, ...contacts],
-//       }));
-//     }
-//   }
-
-//   onDeleteContact = id => {
-//     let indexToDelete = this.state.contacts.findIndex(contact => contact.id === id)
-//     let newContacts = this.state.contacts
-//     newContacts.splice((indexToDelete), 1)
-//     this.setState({contacts: [...newContacts]})
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Title>Phonebook</Title>
-//          <ContactForm handleAdd={this.handleAdd}/>
-
-//         <Title>Contacts</Title>
-//         <Filter handleFilterChange={this.handleFilterChange} value={this.state.filter}/>
-//         <ContactList list={this.state.contacts
-//             .filter(user =>
-//               user.name.toLowerCase().includes(this.state.filter.toLowerCase())
-//             )}
-//             onDeleteContact={this.onDeleteContact}/>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
